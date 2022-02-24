9bbbcb820f7e322371e1f284099341783bb8e352
“Your <you@example.com>
2022-02-24 22:36:06 +0530

    Added Databinding

--- app/src/main/java/com/example/tracexassesment/MainActivity.java
+++ app/src/main/java/com/example/tracexassesment/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.tracexassesment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.tracexassesment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mDataBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDataBinding= DataBindingUtil.setContentView(this, R.layout.activity_main);
         setContentView(R.layout.activity_main);
+
+
     }
 }
