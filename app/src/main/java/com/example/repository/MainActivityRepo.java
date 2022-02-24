dff802d83f886b89cd9a2098d06b739c309139cb
“Your <you@example.com>
2022-02-25 00:10:47 +0530

    retrofit and repository added

--- app/src/main/java/com/example/repository/MainActivityRepo.java
+++ app/src/main/java/com/example/repository/MainActivityRepo.java
@@ -1,4 +1,31 @@
 package com.example.repository;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.model.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivityRepo {
+
+    private static MainActivityRepo instance;
+    private ArrayList<Data> data= new ArrayList<>();
+
+    private static MainActivityRepo getInstance(){
+        if(instance==null){
+            instance=new MainActivityRepo();
+        }
+        return instance;
+    }
+
+    public MutableLiveData<List<Data>> getData(){
+        fetchData();
+        MutableLiveData<List<Data>> mutableLiveData= new MutableLiveData<>();
+        mutableLiveData.setValue(data);
+        return mutableLiveData;
+    }
+
+    private void fetchData() {
+    }
 }
